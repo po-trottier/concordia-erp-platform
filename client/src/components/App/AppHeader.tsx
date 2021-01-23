d57cd491f141089b1711b41a8c471fd4bc59dfb1
Pierre-Olivier Trottier <po.trottier@gmail.com>
2021-01-23 16:33:32 -0500

    Added profile icon with logout button

--- client/src/components/App/AppHeader.tsx
+++ client/src/components/App/AppHeader.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useLocation} from "react-router-dom";
-import {PageHeader} from 'antd';
+import { PageHeader, Avatar, Dropdown, Menu } from 'antd'
+import {UserOutlined} from '@ant-design/icons'
 
 import {Routes} from "../../router/Routes";
 
@@ -9,7 +10,7 @@ export const AppHeader = () => {
 
   // Used to determine the page title. If not page is found, we create a
   // dummy page with a "Page Not Found" title
-  let notFound = Routes.find(r => r.path === location.pathname) ?? {
+  const notFound = Routes.find(r => r.path === location.pathname) ?? {
     path: '*',
     exact: false,
     title: 'Page Not Found',
@@ -17,15 +18,33 @@ export const AppHeader = () => {
     page: null,
   };
 
+  // TODO Replace this with a proper logout method
+  const logOut = () => {
+    console.log('Log out');
+  }
+
+  const dropdown = (
+    <Menu>
+      <Menu.Item onClick={logOut}>
+        Log Out
+      </Menu.Item>
+    </Menu>
+  );
+
   return (
-    <div>
+    <div className="app-header">
       <PageHeader
         className="site-page-header"
         title="ERP Ultimate"
         subTitle={notFound.title}
-        style={{margin: '8px 16px'}} />
+        style={{padding: '16px 0'}} />
+        <Dropdown overlay={dropdown} trigger={['click']} placement="bottomCenter">
+          <Avatar
+            className="ant-dropdown-link"
+            style={{ backgroundColor: '#1890ff' }}
+            size="large"
+            icon={<UserOutlined />} />
+        </Dropdown>
     </div>
-    // TODO: Add "Profile" icon top right for logged in user. On click,
-    // show a menu that gives the option to log out.
   );
 }
