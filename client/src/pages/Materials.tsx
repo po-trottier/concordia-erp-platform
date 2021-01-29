8e4773520af5816f02953669a8d4d5c81a075139
CamilBouzidi <camilbouzidi@hotmail.com>
2021-01-29 18:34:55 -0500

    Materials with table
    Big structure update - using table instead of list.
    Need to add option to track materials.

--- client/src/pages/Materials.tsx
+++ client/src/pages/Materials.tsx
@@ -2,44 +2,78 @@ import React, {useState} from "react";
 import ReactDOM from 'react-dom';
 import {Button} from 'antd';
 import { List, Typography, Divider } from 'antd';
+import { Table, Space } from 'antd';
+
 
 export const Materials = () => {
 
   interface IMaterial {
     name: string;
     quantity: number;
-    weight: number;
     price: number;
   }
 
   const materialsData: IMaterial[] = [];
-  const [materials, setMaterials] = useState<IMaterial[]|undefined>(undefined);
+  const [materials, setMaterials] = useState<IMaterial[]|undefined>([{name: "Metal", quantity: 30, price: 5},{name: "Plastic", quantity: 10, price: 2}]);
   const [materialName, setMaterialName] = useState("");
   const [materialQty, setMaterialQty] = useState();
-  const [materialUnit, setMaterialUnit] = useState();
   const [materialPrice, setMaterialPrice] = useState();
-  materialsData[0] = {name: "cookie", quantity: 1, weight: 1, price: 1};
-  materialsData[1] = {name: "cookie2", quantity: 1, weight: 1, price: 1};
+  const [trackedMat, setTrackedMat] = useState("");
+  materialsData[0] = {name: "Metal", quantity: 30, price: 5};
+  materialsData[1] = {name: "Plastic", quantity: 10, price: 2};
+  
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Quantity Owned',
+      dataIndex: 'quantity',
+      key: 'quantity',
+    },
+    {
+      title: 'Price / Unit ($)',
+      dataIndex: 'price',
+      key: 'price',
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      // eslint-disable-next-line react/display-name
+      render: (text: string, record: IMaterial) => (
+        <Space size="middle">
+          <Button size="small">Buy More</Button>
+        </Space>
+      ),
+    },
+  ];
 
   let updateState = (e:any) =>
   {
     console.log(materials);
+    materialsData.push({name: "Wood", quantity: 3, price: 1})
     setMaterials(materialsData);
-  } 
+  }
+
+  let track = () =>
+  {
+    materials?.forEach(mat => {
+      if (mat.name == trackedMat){
+        return <h1>material found</h1>;
+      }
+    });
+    return <h1>no material tracked</h1>
+  }
   return(
-    <div>    
+    <div> {updateState}    
       <h1>Materials</h1>
-      <Button type="primary" onClick={updateState}>Primary Button</Button>
-      <Divider orientation="left">Available</Divider>
-    <List
-      bordered
-      dataSource={materials}
-      renderItem={item => (
-        <List.Item>
-          {item.name} <h1>Buy</h1>
-        </List.Item>
-      )}
-    />
+      <Divider orientation="left">Current Stock</Divider>
+      <Table columns={columns} dataSource={materials}/>
+      <Divider orientation="left">Additional Options</Divider>
+      <Button type="primary" onClick={updateState}>Create a material</Button>
+      {track()}
     </div>
   )
 }
