11aacf7ee6d9ad7db9b99237a4a51b6db3d5c913
WilliamM-L <w.morin.laberge@gmail.com>
2021-02-01 00:47:20 -0500

    Audit sort of done, must make it look better

--- client/src/components/Logs/Audit.tsx
+++ client/src/components/Logs/Audit.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Button, Checkbox, Form, Select} from "antd";
+import {Button, Card, Checkbox, DatePicker, Dropdown, Form, Menu, Select, Space} from "antd";
+
 
 
 const Audit = () => {
@@ -23,41 +24,96 @@ const Audit = () => {
 
   }
 
+  const { RangePicker } = DatePicker;
+
+  function onChange() {
+
+  }
+
+  function onOk() {
+
+  }
+
+  const exportOptions = (
+    <Menu>
+        <Menu.Item>
+          PDF
+        </Menu.Item>
+        <Menu.Item>
+          CSV
+        </Menu.Item>
+        <Menu.Item>
+          TXT
+        </Menu.Item>
+    </Menu>
+  );
+
   const actionOptions = ['Create', 'Modify', 'Delete'];
   const securityOptions = ['Successful Login', "Failed Login"]
+  const materialsOptions = ['Steel', "Titanium", "Rubber", "Lubricant"]
+  const manufacturedGoodsOptions = ['Frames', "Tires", "Handlebars", "Chains"]
 
   return (
       <Form {...layout} name="audit-form">
-        <h2 {...title}>Global</h2>
-        <Form.Item label="Action" name="action">
-            <Checkbox.Group style={{textAlign: 'center'}} options={actionOptions}/>
-        </Form.Item>
-        <h2 {...title}>User</h2>
-        <Form.Item name='name' label="Name">
-            <Select
-                showSearch
-                style={{ width: 200 }}
-                placeholder="Select a person"
-                optionFilterProp="children"
-                // onChange={onChange}
-                // onFocus={onFocus}
-                // onBlur={onBlur}
-                // onSearch={onSearch}
-                // filterOption={(input, option) =>
-                //     option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                // }
-            >
-            </Select>
-        </Form.Item>
-        <Form.Item label="Security" name="security">
-            <Checkbox.Group options={securityOptions}/>
-        </Form.Item>
-        <h2 {...title}>Inventory</h2>
-        <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
+          <Card>
+              <h2 {...title}>Global</h2>
+              <Form.Item label="Action" name="action">
+                  <Checkbox.Group options={actionOptions}/>
+              </Form.Item>
+          </Card>
+          <Card>
+              <h2 {...title}>User</h2>
+              <Form.Item name='name' label="Name">
+                  <Select
+                      showSearch
+                      style={{ width: 150 }}
+                      placeholder="Select a person"
+                      optionFilterProp="children"
+                      // onChange={onChange}
+                      // onFocus={onFocus}
+                      // onBlur={onBlur}
+                      // onSearch={onSearch}
+                      // filterOption={(input, option) =>
+                      //     option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                      // }
+                  >
+                  </Select>
+              </Form.Item>
+              <Form.Item label="Security" name="security">
+                  <Checkbox.Group options={securityOptions}/>
+              </Form.Item>
+          </Card>
+          <Card>
+              <h2 {...title}>Inventory</h2>
+              <Form.Item label="Materials" name="materials">
+                  <Checkbox.Group style={{textAlign: 'center'}} options={materialsOptions}/>
+              </Form.Item>
+              <Form.Item label="Manufactured Goods" name="manufacturedGoods">
+                  <Checkbox.Group style={{textAlign: 'center'}} options={manufacturedGoodsOptions}/>
+              </Form.Item>
+          </Card>
+          <Card>
+              <h2 {...title}>Setting</h2>
+              <Form.Item label="Date" name="date">
+                  <Space direction="vertical" size={12}>
+                      <RangePicker
+                          showTime={{ format: 'HH:mm' }}
+                          format="YYYY-MM-DD HH:mm"
+                          onChange={onChange}
+                          onOk={onOk}
+                      />
+                  </Space>
+              </Form.Item>
+          </Card>
+          <Form.Item style={{alignItems: 'center', justifyContent:'center'}}>
+              <Form.Item style = {{padding: '2%', fontWeight: 'bold'}} name='create_audit' label="Create Audit">
+                  <Dropdown overlay={exportOptions}>
+                      <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+                          Export as
+                      </a>
+                  </Dropdown>
+              </Form.Item>
+          </Form.Item>
       </Form>
   )
 }
--- client/src/components/Logs/LogList.tsx
+++ client/src/components/Logs/LogList.tsx
@@ -2,11 +2,27 @@ import React from 'react';
 import {Route, useHistory} from 'react-router-dom';
 import {RouteGuard} from '../../router/RouteGuards';
 import {NoPermissions} from '../../pages/NoPermissions';
-
+import {Button, Card, Checkbox, DatePicker, Dropdown, Form, Menu, Select, Space} from "antd";
 
 const LogList = () => {
+
+  const action = 'action'
+  const author = 'author'
+  const target = 'target'
+  const date = 'date'
+
+  function getLogs(){
+    let logs = [{
+
+    },{
+
+    }];
+
+    return <h1>hey</h1>
+  }
+
   return (
-      <h1>loglist</h1>
+    <div>{getLogs}</div>
   )
 }
 
