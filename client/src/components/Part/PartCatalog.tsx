8d80c9d7ec21303718c29a6230f4b5a0d0ad044e
Matthew Kevork <matthew.kevork@hotmail.com>
2021-02-01 19:21:10 -0500

    Refactor PartCatalog similarly to PartInventory

--- client/src/components/Part/PartCatalog.tsx
+++ client/src/components/Part/PartCatalog.tsx
@@ -1,134 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import { Table, Space, Input, InputNumber, Button } from 'antd';
+import { Input, Button } from 'antd';
 
-import { ColumnsType } from 'antd/lib/table';
-import { PartCatalogEntry } from '../../interfaces/PartCatalogEntry';
+import { dummyPartCatalogData } from './PartDummyData';
+import { ResponsiveTable } from '../ResponsiveTable';
 
 export const PartCatalog = () => {
 	const { Search } = Input;
 
-	const [parts, setParts] = useState<PartCatalogEntry[]>([
-		{
-			partId: '6013855b6c24ef1dc4d21125',
-			partName: 'padded_saddle',
-			vendorId: '6016e42a84bff16218374423',
-			vendorName: "Bill's Bike Supply",
-			price: 8.5,
-		},
-		{
-			partId: '601385659f0d6061b89ac171',
-			partName: 'street_frame',
-			vendorId: '6016e42a84bff16218374423',
-			vendorName: "Bill's Bike Supply",
-			price: 46.5,
-		},
-		{
-			partId: '601385659f0d6061b89ac171',
-			partName: 'street_frame',
-			vendorId: '60173d92d1d5cf7e082e6b1a',
-			vendorName: 'Internal',
-			price: 30.0,
-		},
-		{
-			partId: '6013856ba9f14acff30344d8',
-			partName: 'mountain_frame',
-			vendorId: '6016e45c524d74c1d18fa31b',
-			vendorName: 'Mountain Inc.',
-			price: 80.0,
-		},
-		{
-			partId: '601385704569c47eb79ffc28',
-			partName: 'pedals',
-			vendorId: '60173d92d1d5cf7e082e6b1a',
-			vendorName: 'Internal',
-			price: 11.0,
-		},
-		{
-			partId: '601385704569c47eb79ffc28',
-			partName: 'pedals',
-			vendorId: '6016e42a84bff16218374423',
-			vendorName: "Bill's Bike Supply",
-			price: 16.0,
-		},
-		{
-			partId: '601385747ef2ed6535fc0539',
-			partName: 'shock_absorbers',
-			vendorId: '6016e45c524d74c1d18fa31b',
-			vendorName: 'Mountain Inc.',
-			price: 6.5,
-		},
-		{
-			partId: '60138578d3ca261a2f1c24d8',
-			partName: 'handlebars',
-			vendorId: '6016e42a84bff16218374423',
-			vendorName: "Bill's Bike Supply",
-			price: 8.5,
-		},
-	]);
-	const [data, setData] = useState(parts);
+	const [tableData, setTableData] = useState(dummyPartCatalogData);
 	const [searchValue, setSearchValue] = useState('');
 
-	const columns: ColumnsType<PartCatalogEntry> = [
-		{
-			title: 'Part ID',
-			dataIndex: 'partId',
-			key: 'PartId',
-		},
-		{
-			title: 'Part Name',
-			dataIndex: 'partName',
-			key: 'partName',
-			defaultSortOrder: 'ascend',
-			sorter: {
-				compare: (a: PartCatalogEntry, b: PartCatalogEntry) =>
-					a.partName.localeCompare(b.partName),
-			},
-		},
-		{
-			title: 'Vendor ID',
-			dataIndex: 'vendorId',
-			key: 'vendorId',
-		},
-		{
-			title: 'Vendor Name',
-			dataIndex: 'vendorName',
-			key: 'vendorName',
-			sorter: {
-				compare: (a: PartCatalogEntry, b: PartCatalogEntry) =>
-					a.vendorName.localeCompare(b.vendorName),
-			},
-		},
-		{
-			title: 'Price',
-			dataIndex: 'price',
-			key: 'price',
-			sorter: {
-				compare: (a: PartCatalogEntry, b: PartCatalogEntry) =>
-					a.price - b.price,
-			},
-		},
-		{
-			title: 'Order',
-			key: 'order',
-			// eslint-disable-next-line react/display-name
-			render: (text: string, record: PartCatalogEntry) => (
-				<Space size="middle">
-					<InputNumber min={0} defaultValue={0} onChange={() => {}} />
-				</Space>
-			),
-		},
-	];
+	const cols = {
+		partId: 'Part ID',
+		partName: 'Part Name',
+		vendorId: 'Vendor ID',
+		vendorName: 'Vendor Name',
+		price: 'Price',
+	};
 
 	useEffect(() => {
-		let rows = parts;
+		let rows = dummyPartCatalogData;
 		if (searchValue) {
 			rows = rows.filter((part) =>
 				part.partName.toLowerCase().includes(searchValue)
 			);
 		}
 
-		setData(rows);
-	}, [searchValue, parts]);
+		setTableData(rows);
+	}, [searchValue]);
 
 	const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const value = e.target.value.trim().toLowerCase();
@@ -139,12 +38,12 @@ export const PartCatalog = () => {
 		<div>
 			<h2>Parts Catalog</h2>
 			<Search
-				placeholder="input search text"
+				placeholder="Search for part"
 				onChange={onSearch}
-				style={{ width: 200 }}
+				style={{ width: 200, marginBottom: 18 }}
 			/>
-			<Table columns={columns} dataSource={data} />
-			<Button type="primary" onClick={() => {}}>
+			<ResponsiveTable cols={cols} rows={tableData} />
+			<Button type="primary" onClick={() => {}} style={{ margin: 18 }}>
 				Place Order
 			</Button>
 		</div>
