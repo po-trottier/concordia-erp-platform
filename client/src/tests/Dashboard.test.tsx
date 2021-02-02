a5381607023f10638ba197cda7d29bf0d961839e
Pierre-Olivier Trottier <po.trottier@gmail.com>
2021-02-01 23:47:40 -0500

    Fixed React Tests

---
+++ client/src/tests/Dashboard.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
+
+import { Dashboard } from '../pages/Dashboard'
+
+test('renders properly', () => {
+  const { getByText } = render(
+    <Dashboard />
+  );
+
+  expect(getByText(/API Status/i)).toBeInTheDocument();
+});
