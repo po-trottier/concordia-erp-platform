801573f880807ae83280cf1b1a6397e04b0d5a2d
Matthew Kevork <matthew.kevork@hotmail.com>
2021-02-09 17:10:49 -0500

    Refactor parts and api modules to use mongoose

--- server/src/api/api.module.ts
+++ server/src/api/api.module.ts
@@ -1,20 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Part } from '../parts/entities/part.entity';
 import { PartsModule } from '../parts/parts.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mongodb',
-      url: 'mongodb://localhost/ERP_db',
-      synchronize: true,
-      useUnifiedTopology: true,
-      entities: [Part],
-    }),
-    PartsModule,
-  ],
+  imports: [MongooseModule.forRoot('mongodb://localhost/ERP_db'), PartsModule],
   controllers: [ApiController],
 })
 export class ApiModule {}
--- server/src/parts/parts.module.ts
+++ server/src/parts/parts.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PartsService } from './parts.service';
 import { PartsController } from './parts.controller';
-import { PartRepository } from './part.repository';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PartSchema, Part } from './schemas/part.schema';
 
 /**
  * Contains all logic and files related to parts
  */
 @Module({
-  imports: [TypeOrmModule.forFeature([PartRepository])],
+  imports: [
+    MongooseModule.forFeature([{ name: Part.name, schema: PartSchema }]),
+  ],
   controllers: [PartsController],
   providers: [PartsService],
 })
