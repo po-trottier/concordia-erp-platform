4d30a0f547243f9622934923a7bf9cba4fbbb139
Matthew Kevork <matthew.kevork@hotmail.com>
2021-02-09 00:51:39 -0500

    Setup api module with typeorm and import parts module

--- server/src/api/api.module.ts
+++ server/src/api/api.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Part } from '../parts/entities/part.entity';
+import { PartsModule } from '../parts/parts.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/ERP_db',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Part],
+    }),
+    PartsModule,
+  ],
   controllers: [ApiController],
 })
 export class ApiModule {}
