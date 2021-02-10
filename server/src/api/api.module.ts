67601d0d201cd6fac33df65fe9106a2a7e7c879d
Matthew Kevork <matthew.kevork@hotmail.com>
2021-02-09 21:21:35 -0500

    Move parts folder into api folder

--- server/src/api/api.module.ts
+++ server/src/api/api.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
-import { PartsModule } from '../parts/parts.module';
+import { PartsModule } from './parts/parts.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
