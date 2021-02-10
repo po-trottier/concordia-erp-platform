b887e575378003b9f0c7705b95a87df3fde45392
Matthew Kevork <matthew.kevork@hotmail.com>
2021-02-10 00:48:31 -0500

    Add db name env variable to docker-compose, and use env variables for db connection

--- server/src/api/api.module.ts
+++ server/src/api/api.module.ts
@@ -3,8 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ApiController } from './api.controller';
 import { PartsModule } from './parts/parts.module';
 
+const mongoDbUrl = process.env.DB_URL || 'mongodb://localhost:27017';
+const mongoDbName = process.env.DB_NAME || 'ERP_db';
+
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/ERP_db'), PartsModule],
+  imports: [
+    MongooseModule.forRoot(`${mongoDbUrl}/${mongoDbName}`),
+    PartsModule,
+  ],
   controllers: [ApiController],
 })
 export class ApiModule {}
