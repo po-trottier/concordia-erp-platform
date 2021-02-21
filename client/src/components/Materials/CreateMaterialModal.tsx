a2a62c4c0bc8ebae471c2bdef10a758844a3a268
CamilBouzidi <camilbouzidi@hotmail.com>
2021-02-21 16:12:36 -0500

    Added edit + delete button

---
+++ client/src/components/Materials/CreateMaterialModal.tsx
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import 'antd/dist/antd.css';
+import { Form, Input, Button, Modal, Select } from 'antd';
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
+
+export const CreateMaterialModal = () => {
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const formItemLayout = {
+    labelCol: {
+      xs: { span: 24 },
+      sm: { span: 4 },
+    },
+    wrapperCol: {
+      xs: { span: 24 },
+      sm: { span: 20 },
+    },
+  };
+  const formItemLayoutWithOutLabel = {
+    wrapperCol: {
+      xs: { span: 24, offset: 0 },
+      sm: { span: 20, offset: 4 },
+    },
+  };
+
+
+
+  return (
+    <>
+      <Button type="primary" onClick={showModal} style={{ marginTop: 16 }}>
+        Create New Material
+      </Button>
+      <Modal title="Create New Material" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+        <Form name="dynamic_form_item" {...formItemLayoutWithOutLabel}>
+          <Form.Item>
+            <Input placeholder='Material Name'
+              style={{width: '90%', position: 'absolute'}} />
+          </Form.Item>
+          <Form.List name="names">
+
+            {(fields, { add, remove }, { errors }) => (
+              <>
+                {fields.map((field, index) => (
+                  <Form.Item
+                    {...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)}
+                    label={index === 0 ? 'Materials' : ''}
+                    required={false}
+                    key={field.key}
+                  >
+                    <Form.Item
+                      {...field}
+                      validateTrigger={['onChange', 'onBlur']}
+                      noStyle
+                    >
+                      <Select
+                        showSearch
+                        style={{ width: 355 }}
+                        placeholder="Select a material"
+                        optionFilterProp="children"
+                      >
+                      </Select>
+                    </Form.Item>
+                    {fields.length > 1 ? (
+                      <MinusCircleOutlined
+                        className="dynamic-delete-button"
+                        style={{marginLeft: 10}}
+                        onClick={() => remove(field.name)}
+                      />
+                    ) : null}
+                  </Form.Item>
+                ))}
+
+                <Form.Item>
+
+                  <Button
+                    type="dashed"
+                    onClick={() => add()}
+                    style={{ width: '90%', position: 'absolute'}}
+                    icon={<PlusOutlined />}
+                  >
+                    Add Material
+                  </Button>
+                  <Form.ErrorList errors={errors} />
+                </Form.Item>
+              </>
+            )}
+          </Form.List>
+        </Form>
+      </Modal>
+    </>
+  );
+};
