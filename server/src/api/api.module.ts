889c3bbd21d1753ce336830c9373bb1cb5f9d68f
Adrien Tremblay <adrien@adrientremblay.com>
2021-02-23 09:52:19 -0500

    Fix product logs api

--- server/src/api/api.module.ts
+++ server/src/api/api.module.ts
@@ -11,6 +11,7 @@ import { JwtAuthGuard } from './auth/guards/jwt.guard';
 import { RolesGuard } from './roles/roles.guard';
 import { routes } from '../routes';
 import { ProductsModule } from './products/products.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 const mongoDbUrl = process.env.DB_URL || 'mongodb://localhost:27017';
 const mongoDbName = process.env.DB_NAME || 'ERP_db';
@@ -21,6 +22,7 @@ const mongoDbName = process.env.DB_NAME || 'ERP_db';
     MongooseModule.forRoot(`${mongoDbUrl}/${mongoDbName}`, {
       useFindAndModify: false,
     }),
+    EventEmitterModule.forRoot(),
     PartsModule,
     AuthModule,
     UsersModule,
--- server/src/api/products/products.module.ts
+++ server/src/api/products/products.module.ts
@@ -3,12 +3,26 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsController } from './products/products.controller';
 import { ProductsService } from './products/products.service';
 import { Product, ProductSchema } from './products/schemas/products.schema';
+import { ProductLogsController } from './products-logs/product-logs.controller';
+import {
+  ProductLog,
+  ProductLogSchema,
+} from './products-logs/schemas/product-log.schema';
+import { ProductLogsService } from './products-logs/product-logs.service';
+import { ProductQuantityUpdatedListener } from './products-logs/listeners/product-quantity-updated.listener';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+    MongooseModule.forFeature([
+      { name: ProductLog.name, schema: ProductLogSchema },
+    ]),
   ],
-  controllers: [ProductsController],
-  providers: [ProductsService],
+  controllers: [ProductLogsController, ProductsController],
+  providers: [
+    ProductsService,
+    ProductLogsService,
+    ProductQuantityUpdatedListener,
+  ],
 })
 export class ProductsModule {}
