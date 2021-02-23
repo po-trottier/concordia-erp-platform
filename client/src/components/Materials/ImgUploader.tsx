a0840ed2b25600d63691a4c1c2d18c1d8cc1b1f1
CamilBouzidi <camilbouzidi@hotmail.com>
2021-02-23 18:03:20 -0500

    extracted vars

--- client/src/components/Materials/ImgUploader.tsx
+++ client/src/components/Materials/ImgUploader.tsx
@@ -3,20 +3,20 @@ import { Upload, Button, message } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import { UploadChangeParam, UploadFile } from 'antd/lib/upload/interface';
 
+const uf: UploadFile = {
+    uid: 'gold',
+    size: 1,
+    name: 'string',
+    fileName: 'string',
+    lastModified: 1,
+    lastModifiedDate: new Date("5/10/2018"),
+    url: 'string',
+    status: 'error',
+    type: 'png'
+};
+const ufl: UploadFile[] = [uf];
+const [fileList, updateFileList] = useState(ufl);
 export const ImgUploader = () => {
-    const uf: UploadFile = {
-        uid: 'string',
-        size: 1,
-        name: 'string',
-        fileName: 'string',
-        lastModified: 1,
-        lastModifiedDate: new Date("5/10/2018"),
-        url: 'string',
-        status: 'error',
-        type: 'png'
-    };
-    const ufl: UploadFile[] = [uf];
-    const [fileList, updateFileList] = useState(ufl);
     const props = {
         fileList,
         beforeUpload: (file: UploadFile) => {
@@ -31,7 +31,7 @@ export const ImgUploader = () => {
     };
     return (
         <Upload {...props}>
-            <Button icon={<UploadOutlined />}>Upload png only</Button>
+            <Button icon={<UploadOutlined />}>Upload .png only</Button>
         </Upload>
     );
 };
