c19225fbb3d0b889dc33d175ebfc10a556a61a88
NimitJaggi <nimit.jaggi@hotmail.ca>
2021-02-22 23:39:30 -0500

    Fixed validator and styling issues
    Co-Authored-By: Cedric Martens <18620025+MartensCedric@users.noreply.github.com>

--- client/src/components/Parts/CreatePartModal.tsx
+++ client/src/components/Parts/CreatePartModal.tsx
@@ -1,24 +1,60 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import 'antd/dist/antd.css';
-import { InputNumber, Form, Input, Button, Modal, Select } from 'antd';
-import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
+import {Button, Col, Form, Input, InputNumber, Modal, Row, Select} from 'antd';
+import {MinusCircleTwoTone, PlusOutlined} from '@ant-design/icons';
 
-const { Option } = Select;
+const {Option} = Select;
 
 export const CreatePartModal = () => {
 
   const [form] = Form.useForm();
 
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const [materialsData, setPartsData] = useState([{name:'Metal', id:'23'}, {name:'Plastic', id:'42'}, {name:'Wood', id:'22'}]);
+  const [materialsData, setMaterialsData] = useState([{name: 'Metal', id: '23'}, {
+    name: 'Plastic',
+    id: '42'
+  }, {name: 'Wood', id: '22'}]);
 
   const showModal = () => {
     setIsModalVisible(true);
   };
 
-  const handleSubmit = (values : any) => {
+  const hidematerialsError = () => {
+    const materialsError = document.getElementById('display-materials-error');
+    if(materialsError) {
+      materialsError.style.display = 'none';
+    }
+  }
+
+  const displaymaterialsError = () => {
+    const materialsError = document.getElementById('display-materials-error');
+    if(materialsError) {
+      materialsError.style.display = 'block';
+    }
+  };
+
+  const handleSubmit = (values: any) => {
+    let materials = values['list_materials'];
+
+    if (!materials) {
+      displaymaterialsError();
+      return;
+    }
+
+    let hasDefinedmaterial = false;
+    for (let i = 0; i < materials.length; i++) {
+      if (materials[i]) {
+        hasDefinedmaterial = true;
+        break;
+      }
+    }
+    if (!hasDefinedmaterial) {
+      displaymaterialsError();
+      return;
+    }
+
+
     setIsModalVisible(false);
-    console.log(values);
     form.resetFields();
   };
 
@@ -27,99 +63,83 @@ export const CreatePartModal = () => {
     form.resetFields();
   };
 
-  const formItemLayout = {
-    labelCol: {
-      xs: { span: 24 },
-      sm: { span: 4 },
-    },
-    wrapperCol: {
-      xs: { span: 24 },
-      sm: { span: 20 },
-    },
-  };
-  const formItemLayoutWithOutLabel = {
-    wrapperCol: {
-      xs: { span: 24, offset: 0 },
-      sm: { span: 20, offset: 4 },
-    },
-  };
-
-
-
   return (
-    <>
-      <Button type="primary" onClick={showModal} style={{ marginTop: 16 }}>
-        Create New Part
+    <div>
+      <Button type="primary" onClick={showModal} style={{marginTop: 16}}>
+        Create New part
       </Button>
-      <Modal title="Create New Part" visible={isModalVisible} onOk={form.submit} onCancel={handleCancel}>
-        <Form form={form} onFinish={handleSubmit} name="dynamic_form_item" {...formItemLayoutWithOutLabel}>
-          <div style={{display:'flex', alignItems:'center', padding:4}}>
-            <span style={{marginRight:4}}>Part Name : </span>
-            <Form.Item
-              name="part_name"
-              rules={[{ required: true, message: 'Please enter a part name!' }]}>
-              <Input placeholder='Part Name'
-                     style={{width: 335}} />
-            </Form.Item>
-          </div>
-          <Form.List name="list_materials"
-                     rules={[
-                       {
-                         validator: async (_, materials) => {
-                           console.log(materials)
-                           if (!materials || materials.length < 1 || !materials[0]) {
-                             return Promise.reject(new Error('Add at least one material'));
-                           }
-                         },
-                       },
-                     ]}
+      <Modal title="Create New part" visible={isModalVisible} onOk={form.submit} onCancel={handleCancel}>
+        <Form form={form} onFinish={handleSubmit} name="dynamic_form_item">
+          <Row align="middle" style={{marginBottom:16}}>
+            <Col sm={6} span={9}>
+              <span>Part Name : </span>
+            </Col>
+            <Col sm={18} span={15}>
+              <Form.Item
+                style={{marginBottom:0}}
+                name="part_name"
+                rules={[{required: true, message: 'Please enter a part name!'}]}>
+                <Input placeholder='Part Name'/>
+              </Form.Item>
+            </Col>
+          </Row>
+          <Form.List name="list_materials">
 
-          >
-
-            {(fields, { add, remove }, { errors }) => (
-              <>
+            {(fields, {add, remove}, {errors}) => (
+              <div>
                 {fields.map((field, index) => (
-                  <div style={{display:'flex', alignItems:'center', padding:4}} key={index}>
-                    <span style={{marginRight:4}}>Material :</span>
-                    <Form.Item
-                      {...field}
-                      validateTrigger={['onChange', 'onBlur']}
-                      noStyle
-                    >
-                      <Select
-                        showSearch
-                        style={{ width: 355, display:'inline-table' }}
-                        placeholder="Select a material"
-                        optionFilterProp="children"
+                  <Row key={index} align="middle" style={{marginBottom:8}}>
+                    <Col sm={6} span={9} style={{paddingRight:8}}>
+                      Material :
+                    </Col>
+                    <Col className="margin-bottom-mobile"  sm={16} span={13}>
+                      <Form.Item
+                        {...field}
+                        validateTrigger={['onChange', 'onBlur']}
+                        noStyle
                       >
-                        {materialsData.map((material, index) => (<Option key={material.id} value={material.id}>{material.name}</Option>))}
-                      </Select>
-                    </Form.Item>
-                    <MinusCircleOutlined
-                      style={{marginLeft:4}}
-                      className="dynamic-delete-button"
-                      onClick={() => remove(field.name)}
-                    />
-                  </div>
+                        <Select
+                          showSearch
+                          style={{width: '100%', display: 'inline-table'}}
+                          placeholder="Select a material"
+                          optionFilterProp="children"
+                          onChange={hidematerialsError}
+                        >
+                          {materialsData.map((material, index) => (
+                            <Option key={material.id} value={material.id}>{material.name}</Option>))}
+                        </Select>
+                      </Form.Item>
+                    </Col>
+                    <Col sm={2} span={2} style={{textAlign:'right'}}>
+                      <MinusCircleTwoTone
+                        className="dynamic-delete-button"
+                        twoToneColor="red"
+                        onClick={() => remove(field.name)}
+                      />
+                    </Col>
+                  </Row>
                 ))}
-
-                <Form.ErrorList errors={errors} />
-                <Form.Item>
-
-                  <Button
-                    type="dashed"
-                    onClick={() => add()}
-                    style={{width:'-webkit-fill-available', marginLeft:50, marginTop:10}}
-                    icon={<PlusOutlined />}
-                  >
-                    Add Material
-                  </Button>
-                </Form.Item>
-              </>
+                <span id="display-materials-error" style={{color:'red', display:'none'}}>Please add at least one material</span>
+                <Form.ErrorList errors={errors}/>
+                <Row>
+                  <Col span={24}>
+                    <Form.Item style={{marginBottom:0, marginTop:16}}>
+                      <Button
+                        type="dashed"
+                        onClick={() => add()}
+                        style={{width: '100%'}}
+                        icon={<PlusOutlined/>}
+                      >
+                        Add material
+                      </Button>
+                    </Form.Item>
+                  </Col>
+                </Row>
+              </div>
             )}
           </Form.List>
         </Form>
       </Modal>
-    </>
+    </div>
   );
 };
