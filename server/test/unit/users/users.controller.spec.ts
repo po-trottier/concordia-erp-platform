9854b9ec830d051a5d7051198f2e9f59230f7344
Radley Carpio <radleycarpio@live.com>
2021-02-24 16:10:18 -0500

    Completed rest of users controller suite except for update user

--- server/test/unit/users/users.controller.spec.ts
+++ server/test/unit/users/users.controller.spec.ts
@@ -31,12 +31,30 @@ describe('UsersController', () => {
 				jest.spyOn(usersService, 'findAll').mockImplementation(async () => await result);
 
 				expect(await usersController.findAll()).toBe(result);
-			})
+			});
     });
+
+		describe('findOne', () => {
+			it('Should return a user from their username', async () => {
+				const result: User = {
+					firstName: 'System',
+					lastName: 'Administrator',
+					username: 'admin',
+					password: 'Password1!',
+					email: 'test@gmail.com',
+					role: 4,
+				}
+
+				jest.spyOn(usersService, 'findOne').mockImplementation(async () => await result)
+
+				expect(await usersController.findOne(result.username)).toBe(result);
+			});
+		});
+
 
     describe('create', () => {
 			it('Should create a user', async () => {
-				const user: User = {
+				const result: User = {
 					firstName: 'John',
 					lastName: 'Doe',
 					username: 'johnDoe69420',
@@ -46,16 +64,59 @@ describe('UsersController', () => {
 				}
 
 				const newUser = new CreateUserDto();
-				newUser.firstName = 'John';
-				newUser.lastName = 'Doe';
-				newUser.username = 'johnDoe69420';
-				newUser.password = 'Password1!';
-				newUser.email = 'jdoeSniper@noscope.com',
+				newUser.firstName = result.firstName;
+				newUser.lastName = result.lastName;
+				newUser.username = result.username;
+				newUser.password = result.password;
+				newUser.email = result.email,
 				newUser.role = Role.SYSTEM_ADMINISTRATOR;
 				
-				jest.spyOn(usersService, 'create').mockImplementation(async () => await user)
+				jest.spyOn(usersService, 'create').mockImplementation(async () => await result)
 
-				expect(await usersController.create(newUser)).toBe(user);
-			})
+				expect(await usersController.create(newUser)).toBe(result);
+			});
+    });
+
+		describe('remove', () => {
+			it('Should delete a user from their username', async () => {
+				const result: User = {
+					firstName: 'John',
+					lastName: 'Doe',
+					username: 'johnDoe69420',
+					password: 'Password1!',
+					email: 'jdoeSniper@noscope.com',
+					role: 4,
+				}
+
+				jest.spyOn(usersService, 'remove').mockImplementation(async () => await result)
+
+				expect(await usersController.remove(result.username)).toBe(result);
+			});
+		});
+
+
+		describe('update', () => {
+			it('Should update user attribute values', async () => {
+				const result: User = {
+					firstName: 'EPIC System',
+					lastName: 'Admin',
+					username: 'admin',
+					password: 'Password1!',
+					email: 'admin@admin.com',
+					role: 4,
+				}
+
+				const updatedUser = new UpdateUserDto();
+				updatedUser.firstName = result.firstName;
+				updatedUser.lastName = result.lastName;
+				updatedUser.username = result.username;
+				updatedUser.password = result.password;
+				updatedUser.email = result.email,
+				updatedUser.role = 4;
+				
+				jest.spyOn(usersService, 'create').mockImplementation(async () => await result)
+
+				expect(await usersController.update(result.username, updatedUser)).toBe(result);
+			});
     });
 });
