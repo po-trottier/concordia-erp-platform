f5b17822404850408261f665871d91bf229ee5db
Radley Carpio <radleycarpio@live.com>
2021-02-24 18:45:47 -0500

    Fixed imports for parts controller/service test suites

--- server/test/unit/parts/parts.controller.spec.ts
+++ server/test/unit/parts/parts.controller.spec.ts
@@ -1,14 +1,16 @@
-import { PartsController } from '../../../src/api/parts/parts.controller';
-import { PartsService } from '../../../src/api/parts/parts.service';
-import { CreatePartDto } from '../../../src/api/parts/dto/create-part.dto';
-import { UpdatePartDto } from '../../../src/api/parts/dto/update-part.dto';
+import { PartsController } from '../../../src/api/parts/parts/parts.controller';
+import { PartsService } from '../../../src/api/parts/parts/parts.service';
+import { PartLogsService } from '../../../src/api/parts/parts-logs/part-logs.service'
+import { CreatePartDto } from '../../../src/api/parts/parts/dto/create-part.dto';
+import { UpdatePartDto } from '../../../src/api/parts/parts/dto/update-part.dto';
 import { Model } from 'mongoose';
-import { Part, PartDocument } from '../../../src/api/parts/schemas/part.schema';
+import { Part, PartDocument } from '../../../src/api/parts/parts/schemas/part.schema';
 
 describe('PartsController', () => {
   
   let partsController: PartsController;
   let partsService: PartsService;
+  let partLogsService: PartLogsService;
   let partsDocumentModel: Model<PartDocument>;
 
   const dummyPart: Part = {
@@ -18,7 +20,7 @@ describe('PartsController', () => {
   }
 
   beforeEach(async () => {
-    partsService = new PartsService(partsDocumentModel);
+    partsService = new PartsService(partsDocumentModel, partLogsService);
     partsController = new PartsController(partsService);
   });
 
--- server/test/unit/parts/parts.service.spec.ts
+++ server/test/unit/parts/parts.service.spec.ts
@@ -1,13 +1,18 @@
-import { PartsService } from '../../../src/api/parts/parts.service';
+import { PartsController } from '../../../src/api/parts/parts/parts.controller';
+import { PartsService } from '../../../src/api/parts/parts/parts.service';
+import { PartLogsService } from '../../../src/api/parts/parts-logs/part-logs.service'
+import { CreatePartDto } from '../../../src/api/parts/parts/dto/create-part.dto';
+import { UpdatePartDto } from '../../../src/api/parts/parts/dto/update-part.dto';
 import { Model } from 'mongoose';
-import { PartDocument } from '../../../src/api/parts/schemas/part.schema';
+import { Part, PartDocument } from '../../../src/api/parts/parts/schemas/part.schema';
 
 describe('PartsService', () => {
   let partsService: PartsService;
+  let partLogsService: PartLogsService;
   let partsDocumentModel: Model<PartDocument>;
 
   beforeEach(async () => {
-    partsService = new PartsService(partsDocumentModel);
+    partsService = new PartsService(partsDocumentModel, partLogsService);
   });
 
   it('should be defined', () => {
