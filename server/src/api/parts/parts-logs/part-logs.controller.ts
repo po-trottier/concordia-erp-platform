78143738efc44e8ddefb1cc564746a751f00312c
Matthew Kevork <matthew.kevork@hotmail.com>
2021-02-24 18:20:49 -0500

    Add findOne route to part logs controller

--- server/src/api/parts/parts-logs/part-logs.controller.ts
+++ server/src/api/parts/parts-logs/part-logs.controller.ts
@@ -1,15 +1,21 @@
-import { Get, Controller } from '@nestjs/common';
+import { Get, Controller, Param } from '@nestjs/common';
 import { Roles } from '../../roles/roles.decorator';
 import { Role } from '../../roles/roles.enum';
 import { PartLogsService } from './part-logs.service';
 
 @Controller('/logs')
 export class PartLogsController {
-  constructor(private readonly partsLogService: PartLogsService) {}
+  constructor(private readonly partLogsService: PartLogsService) {}
+
+  @Roles(Role.INVENTORY_MANAGER, Role.SYSTEM_ADMINISTRATOR)
+  @Get(':id/:date')
+  findOne(@Param('id') id: string, @Param('date') date: Date) {
+    return this.partLogsService.findOne(id, date);
+  }
 
   @Roles(Role.INVENTORY_MANAGER, Role.SYSTEM_ADMINISTRATOR)
   @Get()
   findAll() {
-    return this.partsLogService.findAll();
+    return this.partLogsService.findAll();
   }
 }
