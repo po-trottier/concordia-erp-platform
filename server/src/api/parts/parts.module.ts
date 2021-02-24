9c821de68c1cf77a402ca0fa1cca1dd3c86e2748
Matthew Kevork <matthew.kevork@hotmail.com>
2021-02-24 16:45:01 -0500

    Update parts module

--- server/src/api/parts/parts.module.ts
+++ server/src/api/parts/parts.module.ts
@@ -3,7 +3,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PartsService } from './parts/parts.service';
 import { PartsController } from './parts/parts.controller';
 import { PartSchema, Part } from './parts/schemas/part.schema';
-import { PartQuantityUpdatedListener } from './parts-logs/listeners/part-quantity-updated.listener';
 import { PartLog, PartLogSchema } from './parts-logs/schemas/part-log.schema';
 import { PartLogsController } from './parts-logs/part-logs.controller';
 import { PartLogsService } from './parts-logs/part-logs.service';
@@ -17,6 +16,6 @@ import { PartLogsService } from './parts-logs/part-logs.service';
     MongooseModule.forFeature([{ name: PartLog.name, schema: PartLogSchema }]),
   ],
   controllers: [PartLogsController, PartsController],
-  providers: [PartsService, PartLogsService, PartQuantityUpdatedListener],
+  providers: [PartsService, PartLogsService],
 })
 export class PartsModule {}
