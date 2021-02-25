4b3586ec6f9f553ca8b6c82c62662a38a446d324
Pierre-Olivier Trottier <po.trottier@gmail.com>
2021-02-25 00:44:48 -0500

    Validate ENV Structure on Startup

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,19 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule } from '@nestjs/config';
 import { ApiModule } from './api/api.module';
+import { validate } from './shared/env';
 
 @Module({
   imports: [
     ApiModule,
+    // ENV Support
+    ConfigModule.forRoot({
+      validate,
+      cache: true,
+      isGlobal: true,
+    }),
     // Use the output of the react build as static assets
     // Relative path: ../../client/build
     ServeStaticModule.forRoot({
