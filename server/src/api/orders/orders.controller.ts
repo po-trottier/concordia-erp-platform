401985688b1ca6f1cf26a03c4f5ad938813716d6
Cedric Martens <cedricmartens98@gmail.com>
2021-03-09 19:28:42 -0500

    Finished summary backend
    Co-authored-by: Bicher123 <bicher.chammaa@mail.concordia.ca>

--- server/src/api/orders/orders.controller.ts
+++ server/src/api/orders/orders.controller.ts
@@ -11,6 +11,9 @@ import { MaterialOrdersService } from './material-orders.service';
 import { CreateMaterialOrderListDto } from './dto/create-material-order-list.dto';
 import { ProductOrdersService } from './product-orders.service';
 import { CreateProductOrderListDto } from './dto/create-product-order-list.dto';
+import { CreateProductOrderDto } from './dto/create-product-order.dto';
+import { CreateMaterialOrderDto } from './dto/create-material-order.dto';
+import { SummaryDto } from './dto/summary.dto';
 
 /**
  * Controller class of the Order entity
@@ -69,5 +72,38 @@ export class OrdersController {
   @Delete('products/:id')
   removeProduct(@Param('id') id: string) {
     return this.productOrderService.remove(id);
+  }
+
+  @Get('summary')
+  async summary() {
+    const productOrders: CreateProductOrderDto[] = await this.productOrderService.findAll();
+    const materialOrders: CreateMaterialOrderDto[] = await this.materialOrderService.findAll();
+
+    const dateMap = new Map<string, number>();
+
+    productOrders.forEach((p) => {
+      const dateOrdered: string = p.dateOrdered.toISOString().split('T')[0];
+      let currentValue: number = dateMap.get(dateOrdered);
+      if (currentValue == null) {
+        currentValue = 0;
+      }
+      dateMap.set(dateOrdered, currentValue + p.amountDue);
+    });
+
+    materialOrders.forEach((m: CreateMaterialOrderDto) => {
+      const dateOrdered: string = m.dateOrdered.toISOString().split('T')[0];
+      let currentValue: number = dateMap.get(dateOrdered);
+      if (currentValue == null) {
+        currentValue = 0;
+      }
+      dateMap.set(dateOrdered, currentValue - m.amountDue);
+    });
+
+    const summaries: SummaryDto[] = [];
+    dateMap.forEach((value: number, key: string) => {
+      summaries.push({ date: key, profit: value });
+    });
+
+    return summaries;
   }
 }
