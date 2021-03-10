6089bf476528ec1cba9b3376050667472e35fe91
Radley Carpio <radleycarpio@live.com>
2021-03-09 22:21:15 -0500

    Added Unit Tests for Locations

---
+++ server/test/unit/locations/locations.controller.spec.ts
@@ -0,0 +1,91 @@
+import { LocationsController } from '../../../src/api/locations/locations.controller';
+import { LocationsService } from '../../../src/api/locations/locations.service';
+import { CreateLocationDto } from '../../../src/api/locations/dto/create-location.dto';
+import { UpdateLocationDto } from '../../../src/api/locations/dto/update-location.dto';
+import { Model } from 'mongoose';
+import { Location, LocationDocument } from '../../../src/api/locations/schemas/location.schema';
+
+describe('FinanceController', () => {
+  let locationsController: LocationsController;
+  let locationsService: LocationsService;
+  let locationDocument: Model<LocationDocument>;
+
+  const dummyLocation: Location = {
+    name: 'Montreal Warehouse',
+    address: '123 Street, Montreal'
+  };
+
+  beforeEach(() => {
+    locationsService = new LocationsService(locationDocument);
+    locationsController = new LocationsController(locationsService);
+  });
+
+  describe('findAll', () => {
+    it('Should return a list of all locations', async () => {
+      const result: Location[] = [dummyLocation];
+      jest
+        .spyOn(locationsService, 'findAll')
+        .mockImplementation(async () => await result);
+
+      expect(await locationsController.findAll()).toBe(result);
+    });
+  });
+
+  describe('findOne', () => {
+    it('Should return a location by its id', async () => {
+      const result: Location = dummyLocation;
+
+      jest
+        .spyOn(locationsService, 'findOne')
+        .mockImplementation(async () => await result);
+
+      expect(await locationsController.findOne('123')).toBe(result);
+    });
+  });
+
+  describe('create', () => {
+    it('Should create a location', async () => {
+      const result: Location = dummyLocation;
+
+      const newLocation = new CreateLocationDto();
+      newLocation.name = result.name;
+      newLocation.address = result.address;
+
+      jest
+        .spyOn(locationsService, 'create')
+        .mockImplementation(async () => await result);
+
+      expect(await locationsController.create(newLocation)).toBe(result);
+    });
+  });
+
+  describe('remove', () => {
+    it('Should delete a location by its id', async () => {
+      const result: Location = dummyLocation;
+
+      jest
+        .spyOn(locationsService, 'remove')
+        .mockImplementation(async () => await result);
+
+      expect(await locationsController.remove('123')).toBe(result);
+    });
+  });
+
+  describe('update', () => {
+    it('Should update location attribute values by its id', async () => {
+      const result: Location = dummyLocation;
+
+      const newLocation = new UpdateLocationDto();
+      newLocation.name = result.name;
+      newLocation.address = result.address;
+      
+      jest
+        .spyOn(locationsService, 'update')
+        .mockImplementation(async () => await result);
+
+      expect(await locationsController.update('123', newLocation)).toBe(
+        result,
+      );
+    });
+  });
+});
