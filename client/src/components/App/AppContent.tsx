3fa7f2d5c66b96df485184f1d2860558d5500166
Pierre-Olivier Trottier <po.trottier@gmail.com>
2021-03-11 17:19:03 -0500

    Removed Force Update
    This will need to be replaced by useEffect Hooks in all components that depend on location

--- client/src/components/App/AppContent.tsx
+++ client/src/components/App/AppContent.tsx
@@ -1,32 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { Card, Layout } from 'antd';
 
 import { Routes } from '../../router/Routes';
 import { NotFound } from '../../pages/NotFound';
 import GuardedRoute from '../Router/GuardedRoute';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../store/Store';
 
 const { Content } = Layout;
 
 export const AppContent = () => {
-
-  const useForceUpdate = () => {
-    const [, forceUpdate] = React.useState(true);
-    return React.useCallback(() => {
-      forceUpdate(s => !s);
-    }, []);
-  };
-  const forceUpdate = useForceUpdate();
-
-  const selected = useSelector((state : RootState) => state.location.selected);
-
-  useEffect(() => {
-    forceUpdate();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selected]);
-
   return (
     <Content className='router-view'>
       <Card>
