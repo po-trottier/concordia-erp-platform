ac0fd98fda60c61785ef3a77f7963c549a952e80
Adrien Tremblay <adrien@adrientremblay.com>
2021-03-14 13:03:32 -0400

    orders state now correctly stores quantity to build for each product

--- client/src/components/Products/ProductCatalog.tsx
+++ client/src/components/Products/ProductCatalog.tsx
@@ -1,37 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import { Button, Card, Input, InputNumber, message, Modal } from 'antd';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { Button, Card, Input, InputNumber, message, Modal } from "antd";
+import { useDispatch, useSelector } from "react-redux";
 
-import { ResponsiveTable } from '../ResponsiveTable';
-import { CreateProductModal } from './CreateProductModal';
-import { EditProductModal } from './EditProductModal';
-import { ProductDetails } from './ProductDetails';
-import { RootState } from '../../store/Store';
-import { ProductEntry } from '../../interfaces/ProductEntry';
-import { ProductStockEntry } from '../../interfaces/ProductStockEntry';
-import { setProductList } from '../../store/slices/ProductListSlice';
-import axios from '../../plugins/Axios';
+import { ResponsiveTable } from "../ResponsiveTable";
+import { CreateProductModal } from "./CreateProductModal";
+import { EditProductModal } from "./EditProductModal";
+import { ProductDetails } from "./ProductDetails";
+import { RootState } from "../../store/Store";
+import { ProductEntry } from "../../interfaces/ProductEntry";
+import { ProductStockEntry } from "../../interfaces/ProductStockEntry";
+import { setProductList } from "../../store/slices/ProductListSlice";
+import axios from "../../plugins/Axios";
 
 const { Search } = Input;
 
 export const ProductCatalog = () => {
   const dispatch = useDispatch();
 
-  const products = useSelector((state : RootState) => state.productList.list);
-  const updated = useSelector((state : RootState) => state.productList.updated);
-  const location = useSelector((state : RootState) => state.location.selected);
+  const products = useSelector((state: RootState) => state.productList.list);
+  const updated = useSelector((state: RootState) => state.productList.updated);
+  const location = useSelector((state: RootState) => state.location.selected);
 
-  const emptyData : {partId: string, quantity: number}[] = [];
-  const [searchValue, setSearchValue] = useState('');
+  const emptyData: { partId: string; quantity: number }[] = [];
+  const [searchValue, setSearchValue] = useState("");
   const [orders, setOrders] = useState(emptyData);
 
   useEffect(() => {
-    axios.get('/products')
+    axios
+      .get("/products")
       .then(({ data }) => {
-        axios.get('/products/stock/' + location)
+        axios
+          .get("/products/stock/" + location)
           .then((resp) => {
-            data.forEach((prod : ProductEntry) => {
-              const entry = resp.data.find((p : ProductStockEntry) => p.productId === prod.id);
+            data.forEach((prod: ProductEntry) => {
+              const entry = resp.data.find(
+                (p: ProductStockEntry) => p.productId === prod.id
+              );
               if (entry) {
                 prod.stock = entry.stock;
               } else {
@@ -41,92 +45,108 @@ export const ProductCatalog = () => {
             dispatch(setProductList(data));
           })
           .catch((err) => {
-            message.error('Something went wrong while getting the products stock.');
+            message.error(
+              "Something went wrong while getting the products stock."
+            );
             console.error(err);
           });
       })
       .catch((err) => {
-        message.error('Something went wrong while getting the products catalog.');
+        message.error(
+          "Something went wrong while getting the products catalog."
+        );
         console.error(err);
       });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [updated]);
 
-  const onSearch = (e : React.ChangeEvent<HTMLInputElement>) => {
+  const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchValue(e.target.value);
   };
 
   const changeBuildAmount = (partId: string, quantity: number) => {
-    setOrders(orders.concat({partId, quantity}));
+    const foundOrder = orders.find((order: { partId: string; quantity: number })=> order.partId === partId);
+    if (foundOrder) {
+      foundOrder.quantity = quantity;
+      setOrders(orders);
+    } else {
+      setOrders(orders.concat({ partId, quantity }));
+    }
     console.log(orders);
-  }
+  };
 
   const getProducts = () => {
     let rows = JSON.parse(JSON.stringify(products));
 
-    if (searchValue.trim() !== '') {
-      rows = rows.filter(
-        (r : ProductEntry) => r.name.trim().toLowerCase().includes(searchValue.trim().toLowerCase()));
+    if (searchValue.trim() !== "") {
+      rows = rows.filter((r: ProductEntry) =>
+        r.name.trim().toLowerCase().includes(searchValue.trim().toLowerCase())
+      );
     }
-    for (let i =0; i < rows.length; i++){
+    for (let i = 0; i < rows.length; i++) {
       let row = rows[i];
-      row.id = row['_id'];
+      row.id = row["_id"];
       row.details = (
-        <Button type='primary' size='small' onClick={() => showModal(row)}>
+        <Button type="primary" size="small" onClick={() => showModal(row)}>
           See Details
         </Button>
       );
       row.build = (
-        <InputNumber onChange={(value: any)=> changeBuildAmount(row.id, value)}
-          placeholder='Input a quantity'
+        <InputNumber
+          onChange={(value: any) => changeBuildAmount(row.id, value)}
+          placeholder="Input a quantity"
           min={0}
-          style={{ width: '100%' }} />
+          style={{ width: "100%" }}
+        />
       );
-      row.actions = (
-        <EditProductModal product={row} />
-      );
+      row.actions = <EditProductModal product={row} />;
     }
 
-    rows.sort((a : ProductEntry, b : ProductEntry) => {
+    rows.sort((a: ProductEntry, b: ProductEntry) => {
       return a.name < b.name ? -1 : 1;
     });
     return rows;
   };
 
-  const showModal = (row : ProductEntry) => {
+  const showModal = (row: ProductEntry) => {
     Modal.info({
-      title: 'Product Details',
-      content: <ProductDetails product={row} />
+      title: "Product Details",
+      content: <ProductDetails product={row} />,
     });
   };
 
   const columns = {
-    name: 'Name',
-    details: 'Details',
-    price: 'Price',
-    stock: 'Stock',
-    actions: 'Actions',
-    build: 'Build'
+    name: "Name",
+    details: "Details",
+    price: "Price",
+    stock: "Stock",
+    actions: "Actions",
+    build: "Build",
   };
 
   const buildProducts = () => {
     console.log(products);
-  }
+  };
 
   return (
     <div>
-      <Card style={{ margin: '24px 0' }}>
+      <Card style={{ margin: "24px 0" }}>
         <Search
-          placeholder='Search for a product'
+          placeholder="Search for a product"
           onChange={onSearch}
-          style={{ marginBottom: 18 }} />
-        {
-          getProducts().length > 0 ?
-            <ResponsiveTable rows={getProducts()} cols={columns} /> :
-            <span>No products were found.</span>
-        }
+          style={{ marginBottom: 18 }}
+        />
+        {getProducts().length > 0 ? (
+          <ResponsiveTable rows={getProducts()} cols={columns} />
+        ) : (
+          <span>No products were found.</span>
+        )}
       </Card>
-      <Button onClick={buildProducts} type='primary' style={{ marginTop: 16, float: 'right' }}>
+      <Button
+        onClick={buildProducts}
+        type="primary"
+        style={{ marginTop: 16, float: "right" }}
+      >
         Build Products
       </Button>
       <CreateProductModal />
