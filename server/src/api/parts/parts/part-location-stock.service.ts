5a33c4aace977fd7782bdad3c20c4997bbba9b9e
Adrien Tremblay <adrien@adrientremblay.com>
2021-03-16 15:33:22 -0400

    remove transaction code from partLocationStock service

--- server/src/api/parts/parts/part-location-stock.service.ts
+++ server/src/api/parts/parts/part-location-stock.service.ts
@@ -103,9 +103,6 @@ export class PartLocationStockService {
       }
     }
 
-    const session = await this.partLocationStockModel.startSession();
-    session.startTransaction();
-
     const updatedPartLocationStock = await this.partLocationStockModel.findOneAndUpdate(
       { partId, locationId },
       { $inc: { stock: netStockChange } },
@@ -124,8 +121,6 @@ export class PartLocationStockService {
 
       await this.partLogsService.update(updatePartLogDto);
     }
-
-    session.endSession();
 
     return this.validatePartLocationStockFound(
       updatedPartLocationStock,
