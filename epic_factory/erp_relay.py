d34fb2a84a820effacdbe3e9b8e1855add057e1b
Cedric Martens <cedricmartens98@gmail.com>
2021-04-04 14:45:55 -0400

    Guess I added a request

--- epic_factory/erp_relay.py
+++ epic_factory/erp_relay.py
@@ -1,7 +1,14 @@
 import serial
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
-running = True
+import requests
+
+running = False
+partId = '604cee081ba2430c10eeee6b'
+url = 'https://localhost/api/products/build/'
+
+
+requests.patch(url, data={'stockbuilt':5}, headers={})
 
 while running:
     number_frames = int(input("Enter the number of bicycle frames to manufacture : "))
