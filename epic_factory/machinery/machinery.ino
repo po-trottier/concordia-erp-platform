d845ec99041d2a33a763485de2454c733195b89e
Cedric Martens <cedricmartens98@gmail.com>
2021-04-05 21:22:15 -0400

    Updated text

--- epic_factory/machinery/machinery.ino
+++ epic_factory/machinery/machinery.ino
@@ -3,21 +3,16 @@
 // Code specifically for a bike frame building machine
 // Runs on an arduino Uno
 
-String part_id = "bicycle frame";
-int buttonPin = 2;
 int redLed = 2;
 int yellowLed = 7;
 int greenLed = 8;
-boolean buttonState = LOW;
 
-boolean debounce(boolean);
 void manufacture();
 
 void setup() {
   pinMode(redLed, OUTPUT);
   pinMode(yellowLed, OUTPUT);
   pinMode(greenLed, OUTPUT);
-  //pinMode(buttonPin, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
 }
@@ -59,41 +54,9 @@ void loop()
     }
   }
 
-  //if(debounce(buttonState) == HIGH && buttonState == LOW)
-  //{
-  //  buttonState = HIGH;
-  //  buttonPressed();
-  //}
-  //else if(debounce(buttonState) == LOW && buttonState == HIGH)
-  //{
-  //  buttonState = LOW;
-  //  buttonReleased();
-  //}
   digitalWrite(redLed, HIGH);
   digitalWrite(yellowLed, LOW);
   digitalWrite(greenLed, LOW);
-}
-
-void buttonPressed(void)
-{
-
-}
-
-void buttonReleased(void)
-{
-
-}
-
-boolean debounce(boolean state)
-{
-  boolean stateNow = digitalRead(buttonPin);
-  if(state != stateNow)
-  {
-    delay(10);
-    stateNow = digitalRead(buttonPin);
-  }
-
-  return stateNow;
 }
 
 void manufacture()
@@ -101,17 +64,17 @@ void manufacture()
   // https://www.instructables.com/Build-a-Bicycle-Frame/
   Serial.println("Starting a new bike frame");
   delay(800);
-  Serial.println("Tube mitering...");
+  Serial.println("Doing tube mitering...");
   delay(800);
-  Serial.println("Chain Stays...");
+  Serial.println("Doing chain stays...");
   delay(800);
-  Serial.println("Weld braze...");
+  Serial.println("Welding the front triangle...");
   delay(800);
-  Serial.println("Miter and attach the chain stays...");
+  Serial.println("Mitering and attaching the chain stays...");
   delay(800);
-  Serial.println("Attach the seat stays...");
+  Serial.println("Attaching the seat stays...");
   delay(800);
-  Serial.println("Braze on and bridges...");
+  Serial.println("Adding braze ons to the frame...");
   delay(800);
   Serial.println("Cleaning and painting...");
   delay(800);
