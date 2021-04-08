14cfc7c6ef2cbfe0fe801ebf5581d7be3bcb3814
Radley Carpio <radleycarpio@live.com>
2021-04-07 21:38:47 -0400

    Updated Auth Controller Unit Tests

--- server/test/unit/auth/auth.controller.spec.ts
+++ server/test/unit/auth/auth.controller.spec.ts
@@ -1,6 +1,7 @@
 import { AuthController } from '../../../src/api/auth/auth.controller';
 import { AuthService } from '../../../src/api/auth/auth.service';
 import { LoginAuthDto } from '../../../src/api/auth/dto/login-auth.dto';
+import { PasswordForgottenDto } from '../../../src/api/auth/dto/password-forgotten.dto';
 import { UserDocument } from '../../../src/api/users/schemas/user.schema';
 import { JwtService } from '@nestjs/jwt';
 import { Model } from 'mongoose';
@@ -11,6 +12,16 @@ describe('AuthController', () => {
   let userDocument: Model<UserDocument>;
   let jwtService: JwtService;
 
+  const dummyResult = {
+    firstName: 'System',
+    lastName: 'Administrator',
+    username: 'admin',
+    password: 'Password1!',
+    email: 'test@gmail.com',
+    role: null,
+    token: '123',
+  };
+
   beforeEach(async () => {
     authService = new AuthService(userDocument, jwtService);
     authController = new AuthController(authService);
@@ -18,15 +29,7 @@ describe('AuthController', () => {
 
   describe('login', () => {
     it('Should return the currently logged in user.', async () => {
-      const result = {
-        firstName: 'System',
-        lastName: 'Administrator',
-        username: 'admin',
-        password: 'Password1!',
-        email: 'test@gmail.com',
-        role: null,
-        token: '123',
-      };
+      const result = dummyResult;
 
       const loginAuthDto = new LoginAuthDto();
       loginAuthDto.username = result.username;
@@ -37,6 +40,23 @@ describe('AuthController', () => {
         .mockImplementation(async () => await result);
 
       expect(await authController.login(loginAuthDto)).toBe(result);
+    });
+  });
+
+  describe('generate', () => {
+    it('Should generate a request to send an email.', async () => {
+      const result = {
+        result: 'Password reset email sent to ' + dummyResult.email + ' successfully.'
+      };
+
+      const passwordForgottenDto = new PasswordForgottenDto();
+      passwordForgottenDto.email = dummyResult.email;
+
+      jest
+        .spyOn(authService, 'generate')
+        .mockImplementation(async () => await result);
+
+      expect(await authController.generate(passwordForgottenDto)).toBe(result);
     });
   });
 });
