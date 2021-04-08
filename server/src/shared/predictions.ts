9b869f589bb5a01985665f43db3367f2b3786f28
Adrien Tremblay <adrien@adrientremblay.com>
2021-04-07 22:15:13 -0400

    refactor predictions main algo

--- server/src/shared/predictions.ts
+++ server/src/shared/predictions.ts
@@ -8,21 +8,63 @@ export const addPredictions = (rows: any, id: string) => {
     } else if (buffer[buffer.length - 1][id] === row[id]) {
       buffer.push(row);
     } else {
-      predictions.push(buffer[buffer.length - 1]);
-      predictions.push(
-        getPredictionValue(buffer[0], buffer[buffer.length - 1]),
-      );
+      predictions.push(getCopy(buffer[buffer.length - 1], id));
+      predictions.push(getPredictionValue(buffer[0], buffer[buffer.length - 1], id));
       buffer = [row];
     }
   }
 
+  console.log(predictions);
   return [...rows, ...predictions];
 };
 
-const getPredictionValue = (start, end) => {
-  const prediction = end;
+const getPredictionValue = (start, end, id) => {
+  // actual logic to calculate predictedStock;
+  const firstDate = new Date(start.date);
+  const lastDate = new Date(end.date);
+  const endOfYear = new Date(new Date().getFullYear(), 11, 31);
+  const daysBetween = calculateDiffInDays(firstDate, lastDate);
+  const daysTillEnd = calculateDiffInDays(lastDate, endOfYear);
+  const predictedStockMultiplier =
+    (end.stock - start.stock) / daysBetween;
+  const predictedStock = predictedStockMultiplier * daysTillEnd;
+  const stockDifference = predictedStock - end.stock;
 
-  // TODO Calculate the prediction and change date to End of Year
-
+  // creating prediction row
+  const prediction = {
+    _id: end._id,
+    date: endOfYear.toLocaleString().split(',')[0],
+    locationId: end.locationId,
+    stockBuilt: stockDifference > 0 ? stockDifference : 0,
+    stockUsed: stockDifference < 0 ? -stockDifference : 0,
+    stock: predictedStock,
+    isEstimate: true,
+  };
+  prediction[id] = end[id];
+  if (id === 'materialId')
+    prediction['stockBought'] = prediction.stockBuilt;
   return prediction;
+
+};
+
+const getCopy = (row, id) => {
+  const copy = {
+    _id: row._id,
+    date: row.date,
+    locationId: row.locationId,
+    stockBuilt: row.stockBuilt,
+    stockUsed: row.stockUsed,
+    stock: row.stock,
+    isEstimate: true,
+    isCopy: true
+  }
+  copy[id] = row[id];
+
+  return copy
+}
+
+const calculateDiffInDays = (dateA: Date, dateB: Date) => {
+  const differenceInTime = dateA.getTime() - dateB.getTime();
+  const differenceInDays = Math.round(differenceInTime / (1000 * 3600 * 24));
+  return Math.abs(differenceInDays);
 };
